55f00bc2a8c9df054fa524a995d28e1edaa6dbad
pankaj-cod <upadhyaypankaj322@gmail.com>
2026-05-14 12:41:22 +0530

    chore: explicitly allow unsafe-eval in CSP to support three.js WebGL compilation

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https: http:;"
+          }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
